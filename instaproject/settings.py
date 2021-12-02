0d0bbc6795fe092317977dbf9f91e8b705dd590c
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2021-12-02 11:02:10 +0300

    modified settings.py

--- instaproject/settings.py
+++ instaproject/settings.py
@@ -33,6 +33,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'instaapp.apps.InstaappConfig',
+    'bootstrap4',
+    'cloudinary',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
