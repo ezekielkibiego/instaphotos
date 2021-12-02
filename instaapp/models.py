c45ef0371c0c8c55b2f5aeca1bd7b47de395b76f
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2021-12-02 18:53:06 +0300

    added images class

--- instaapp/models.py
+++ instaapp/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+import datetime as dt
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+
+class Images(models.Model):
+    name = models.CharField(max_length=50)
+    image = CloudinaryField('image')
+    caption = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save_image(self):
+        self.save()
+
+
+    # update image
+    def update_caption(self, name, description):
+        self.name = name
+        self.description = description
+    
+        self.save()
+        
+
+    @classmethod
+    def get_all_images(cls):
+        images = Images.objects.all()
+        return images
+
+    def delete_image(self):
+        self.delete()
+
+    
+
+    def __str__(self):
+        return self.name
--- instaapp/views.py
+++ instaapp/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+from .models import Images
 
 
 def welcome(request):
     return HttpResponse('Welcome to Instaphotos')
 
 def index(request):
-    return render(request, 'all-insta/index.html')
+
+    image = Images.objects.all().order_by('-id')
+
+    return render(request, 'all-insta/index.html',{'image':image})
+   
