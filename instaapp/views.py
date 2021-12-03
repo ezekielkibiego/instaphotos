1fa62d9f94f0d337af2d2433f056c98e7408c975
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2021-12-03 09:38:37 +0300

    added log in form

--- instaapp/views.py
+++ instaapp/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
 from .models import Images
-
+from django.contrib.auth.decorators import login_required
 
 def welcome(request):
     return HttpResponse('Welcome to Instaphotos')
 
+@login_required(login_url='/accounts/login/')
 def index(request):
 
     Image = Images.objects.all().order_by('-id')
--- instaproject/urls.py
+++ instaproject/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.contrib.auth import views 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'',include('instaapp.urls'))
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'',include('instaapp.urls')),
+    url('logout/', views.LogoutView.as_view(), name='logout'),
+    
 ]
