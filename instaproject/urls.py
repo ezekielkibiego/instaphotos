31fba9b36c72b3d2306386469d1c88773610255b
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2021-12-03 17:58:02 +0300

    added login function

--- instaproject/urls.py
+++ instaproject/urls.py
@@ -40,7 +40,7 @@ urlpatterns = [
          name='django_registration_register'),
     path('accounts/', include('django_registration.backends.one_step.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('logout/', views.logout_then_login, name='logout'),
     # path('logout/', views.redirect_to_login, name='logout'),
     
     
