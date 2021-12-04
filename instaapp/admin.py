35998e0523df25eefbb72f4572e3dd44fff1756e
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2021-12-05 00:20:54 +0300

    modified admin.py

--- instaapp/admin.py
+++ instaapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Comments, Images, Likes,Profile
+from .models import Comments, Images, Likes,Profile,Profiles
 
 admin.site.register(Images)
 admin.site.register(Profile)
 admin.site.register(Likes)
 admin.site.register(Comments)
+admin.site.register(Profiles)
