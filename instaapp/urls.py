fa69af142fe545b5654af72afcea913aee4227b9
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2021-12-06 09:14:05 +0300

    modified urls

--- instaapp/urls.py
+++ instaapp/urls.py
@@ -1,11 +1,15 @@
-# from django.conf.urls import url
-from django.urls import path
+from django.urls import path,re_path
 from django.contrib import admin
 from .import views
+
 
 
 urlpatterns = [
     path('',views.index,name = 'index'),
     path('',views.welcome,name = 'welcome'),
+    path('profile/', views.profile, name='profile'),
+    path('like/', views.like_image, name='like-image'),
+    
+
 
 ]
