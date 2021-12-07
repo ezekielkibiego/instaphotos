203a0ca4430ce4f7689a143d0bc36c8e9557adda
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2021-12-07 11:35:14 +0300

    added image test case

--- instaapp/tests.py
+++ instaapp/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
-# Create your tests here.
+from instaapp.models import Image,User
+
+class ImageTestCase(TestCase):
+    def setUp(self):
+        # create a user
+        user = User.objects.create(
+            username='test_user',
+           
+        )
+        Image.objects.create(
+            photo_name='test_image',
+            image='https://upload.wikimedia.org/wikipedia/commons/4/49/Flag_of_Kenya.svg',
+            photo_caption='test image',
+            
+            user_id=user.id
+        )
+
+    def test_image_name(self):
+        image = Image.objects.get(photo_name='test_image')
+        self.assertEqual(image.photo_name, 'test_image')
