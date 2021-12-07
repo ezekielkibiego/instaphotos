a70e44bedc158ddb62d7c545efccf2c02a6a9b0d
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2021-12-07 12:55:49 +0300

    added test liked

--- instaapp/tests.py
+++ instaapp/tests.py
@@ -67,3 +67,14 @@ class ImageTestCase(TestCase):
             
             user_id=user.id
         )
+    def test_image_liked(self):
+        user = User.objects.create(
+            username='newuser',
+            
+        )
+        photo = Image.objects.create(
+            photo_caption='test post',
+            image='https://upload.wikimedia.org/wikipedia/commons/4/49/Flag_of_Kenya.svg',
+            
+            user_id=user.id
+        )
