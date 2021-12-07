8f7b4fec1542a2cfb584da5c6a63d9cd0d9807e6
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2021-12-07 10:32:48 +0300

    modified admin.py

--- instaapp/admin.py
+++ instaapp/admin.py
@@ -6,3 +6,12 @@ admin.site.register(Profile)
 admin.site.register(Like)
 admin.site.register(Comment)
 admin.site.register(Follow)
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'created_on', 'active')
+    list_filter = ('active', 'created_on')
+    search_fields = ('name', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
